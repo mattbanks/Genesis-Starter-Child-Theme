170f0f244078828645d1c310c42b0c93f98bdcf5
Matt Banks <mjbanks@gmail.com>
2013-02-07 14:51:40 -0500

    code cleanup, moved functions out to theme-functions, hooks and filters now in functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Functions
- */
 
 /**
  * Theme Setup
@@ -16,7 +13,16 @@
 add_action('genesis_setup','child_theme_setup', 15);
 function child_theme_setup() {
 
-	// ** Backend **
+	/****************************************
+	Setup child theme functions
+	*****************************************/
+
+	include_once( CHILD_DIR . '/lib/theme-functions.php' );
+
+
+	/****************************************
+	Backend
+	*****************************************/
 
 	// Image Sizes
 	// add_image_size( $name, $width = 0, $height = 0, $crop = false );
@@ -60,17 +66,14 @@ function child_theme_setup() {
 	// Editor Styles
 	add_editor_style( 'editor-style.css' );
 
-	// Setup Theme Settings
-	include_once( CHILD_DIR . '/lib/child-theme-settings.php' );
-
 	// Remove Genesis Theme Settings Metaboxes
 	add_action( 'genesis_theme_settings_metaboxes', 'mb_remove_genesis_metaboxes' );
 
 	// Don't update theme
 	add_filter( 'http_request_args', 'mb_dont_update_theme', 5, 2 );
 
-	// Setup Theme Functions
-	include_once( CHILD_DIR . '/lib/theme-functions.php' );
+	// Setup Child Theme Settings
+	include_once( CHILD_DIR . '/lib/child-theme-settings.php' );
 
 	// Set Content Width
 	$content_width = apply_filters( 'content_width', 740, 740, 1140 );
@@ -81,7 +84,26 @@ function child_theme_setup() {
 	// Add support for custom header
 	add_theme_support( 'genesis-custom-header', array( 'width' => 1140, 'height' => 100 ) );
 
-	// ** Frontend **
+	// Remove Dashboard Meta Boxes
+	add_action('wp_dashboard_setup', 'mb_remove_dashboard_widgets' );
+
+	// Change Admin Menu Order
+	add_filter('custom_menu_order', 'mb_custom_menu_order');
+	add_filter('menu_order', 'mb_custom_menu_order');
+
+	// Hide Admin Areas that are not used
+	add_action( 'admin_menu', 'mb_remove_menu_pages' );
+
+	// Remove default link for images
+	add_action('admin_init', 'mb_imagelink_setup', 10);
+
+	// Show Kitchen Sink in WYSIWYG Editor
+	add_filter( 'tiny_mce_before_init', 'mb_unhide_kitchensink' );
+
+
+	/****************************************
+	Frontend
+	*****************************************/
 
 	// HTML5 doctype
 	remove_action( 'genesis_doctype', 'genesis_do_doctype' );
@@ -89,9 +111,6 @@ function child_theme_setup() {
 
 	// Load custom favicon to header
 	add_filter( 'genesis_pre_load_favicon', 'mb_custom_favicon_filter' );
-	function mb_custom_favicon_filter( $favicon_url ) {
-		return get_stylesheet_directory_uri() . '/images/favicon.ico';
-	}
 
 	// Remove Edit link
 	add_filter( 'genesis_edit_post_link', '__return_false' );
@@ -105,121 +124,26 @@ function child_theme_setup() {
 
 	// Enqueue Scripts
 	add_action( 'wp_enqueue_scripts', 'mb_scripts' );
+
+	// Remove Query Strings From Static Resources
+	add_filter('script_loader_src', 'mb_remove_script_version', 15, 1);
+	add_filter('style_loader_src', 'mb_remove_script_version', 15, 1);
+
+	// Remove Read More Jump
+	add_filter('the_content_more_link', 'mb_remove_more_jump_link');
+
+
+	/****************************************
+	Theme Views
+	*****************************************/
+
+	include_once( CHILD_DIR . '/lib/theme-views.php' );
 }
 
-// ** Backend Functions ** //
 
-/**
- * Customize Contact Methods
- * @since 1.0.0
- *
- * @author Bill Erickson
- * @link http://sillybean.net/2010/01/creating-a-user-directory-part-1-changing-user-contact-fields/
- *
- * @param array $contactmethods
- * @return array
- */
-function mb_contactmethods( $contactmethods ) {
-	unset( $contactmethods['aim'] );
-	unset( $contactmethods['yim'] );
-	unset( $contactmethods['jabber'] );
+/****************************************
+Misc Theme Functions
+*****************************************/
 
-	return $contactmethods;
-}
-
-/**
- * Remove Genesis Theme Settings Metaboxes
- *
- * @since 1.0.0
- * @param string $_genesis_theme_settings_pagehook
- */
-function mb_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
-	//remove_meta_box( 'genesis-theme-settings-feeds',      $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-header',     $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-nav',        $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-breadcrumb', $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-comments',   $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-posts',      $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-blogpage',   $_genesis_theme_settings_pagehook, 'main' );
-	//remove_meta_box( 'genesis-theme-settings-scripts',    $_genesis_theme_settings_pagehook, 'main' );
-}
-
-/**
- * Don't Update Theme
- * @since 1.0.0
- *
- * If there is a theme in the repo with the same name,
- * this prevents WP from prompting an update.
- *
- * @author Mark Jaquith
- * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
- *
- * @param array $r, request arguments
- * @param string $url, request url
- * @return array request arguments
- */
-function mb_dont_update_theme( $r, $url ) {
-	if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
-		return $r; // Not a theme update request. Bail immediately.
-	$themes = unserialize( $r['body']['themes'] );
-	unset( $themes[ get_option( 'template' ) ] );
-	unset( $themes[ get_option( 'stylesheet' ) ] );
-	$r['body']['themes'] = serialize( $themes );
-	return $r;
-}
-
-// ** Frontend Functions ** //
-
-/**
- * HTML5 DOCTYPE
- * removes the default Genesis doctype, adds new html5 doctype
-*/
-function mb_html5_doctype() {
-?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<?php
-}
-
-/**
- * Viewport Meta Tag for Mobile Browsers
- *
- * @author Bill Erickson
- * @link http://www.billerickson.net/code/responsive-meta-tag
- */
-function mb_viewport_meta_tag() {
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-}
-
-/**
- * Footer
- */
-function mb_footer() {
-	echo '<div class="one-half first" id="footer-left">' . wpautop( genesis_get_option( 'footer-left', 'child-settings' ) ) . '</div>';
-	echo '<div class="one-half" id="footer-right">' . wpautop( genesis_get_option( 'footer-right', 'child-settings' ) ) . '</div>';
-}
-
-/**
- * Enqueue Script
- */
-function mb_scripts() {
-	if ( !is_admin() ) {
-    	// Modernizr
-		wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js', false, NULL );
-		// Custom plugins and scripts
-		wp_enqueue_script('customplugins', get_stylesheet_directory_uri() . '/js/plugins.min.js', array('jquery'), NULL, true );
-		wp_enqueue_script('customscripts', get_stylesheet_directory_uri() . '/js/main.min.js', array('jquery'), NULL, true );
-	}
-}
-
-/**
- * Unregister the superfish scripts
- * Can't run in child_theme_setup
- */
+// Unregister the superfish scripts
 add_action( 'wp_enqueue_scripts', 'mb_unregister_superfish' );
-function mb_unregister_superfish() {
-	wp_deregister_script( 'superfish' );
-	wp_deregister_script( 'superfish-args' );
-}
--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -1,6 +1,68 @@
 <?php
 
-// Remove Dashboard Meta Boxes
+/****************************************
+Backend Functions
+*****************************************/
+
+/**
+ * Customize Contact Methods
+ * @since 1.0.0
+ *
+ * @author Bill Erickson
+ * @link http://sillybean.net/2010/01/creating-a-user-directory-part-1-changing-user-contact-fields/
+ *
+ * @param array $contactmethods
+ * @return array
+ */
+function mb_contactmethods( $contactmethods ) {
+	unset( $contactmethods['aim'] );
+	unset( $contactmethods['yim'] );
+	unset( $contactmethods['jabber'] );
+
+	return $contactmethods;
+}
+
+/**
+ * Remove Genesis Theme Settings Metaboxes
+ */
+function mb_remove_genesis_metaboxes( $_genesis_theme_settings_pagehook ) {
+	//remove_meta_box( 'genesis-theme-settings-feeds',      $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-header',     $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-nav',        $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-breadcrumb', $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-comments',   $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-posts',      $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-blogpage',   $_genesis_theme_settings_pagehook, 'main' );
+	//remove_meta_box( 'genesis-theme-settings-scripts',    $_genesis_theme_settings_pagehook, 'main' );
+}
+
+/**
+ * Don't Update Theme
+ * @since 1.0.0
+ *
+ * If there is a theme in the repo with the same name,
+ * this prevents WP from prompting an update.
+ *
+ * @author Mark Jaquith
+ * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
+ *
+ * @param array $r, request arguments
+ * @param string $url, request url
+ * @return array request arguments
+ */
+function mb_dont_update_theme( $r, $url ) {
+	if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
+		return $r; // Not a theme update request. Bail immediately.
+	$themes = unserialize( $r['body']['themes'] );
+	unset( $themes[ get_option( 'template' ) ] );
+	unset( $themes[ get_option( 'stylesheet' ) ] );
+	$r['body']['themes'] = serialize( $themes );
+	return $r;
+}
+
+/**
+ * Remove Dashboard Meta Boxes
+ */
 function mb_remove_dashboard_widgets() {
 	global $wp_meta_boxes;
 	// unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
@@ -12,62 +74,122 @@ function mb_remove_dashboard_widgets() {
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 }
-add_action('wp_dashboard_setup', 'mb_remove_dashboard_widgets' );
 
-
-// Hide Admin Areas that are not used
-function mb_remove_menu_pages() {
-	remove_menu_page('link-manager.php');
-	// remove_menu_page('edit-comments.php');
+/**
+ * Change Admin Menu Order
+ */
+function mb_custom_menu_order($menu_ord) {
+	if (!$menu_ord) return true;
+	return array(
+		// 'index.php', // Dashboard
+		// 'separator1', // First separator
+		// 'edit.php?post_type=page', // Pages
+		// 'edit.php', // Posts
+		// 'upload.php', // Media
+		// 'gf_edit_forms', // Gravity Forms
+		// 'genesis', // Genesis
+		// 'edit-comments.php', // Comments
+		// 'separator2', // Second separator
+		// 'themes.php', // Appearance
+		// 'plugins.php', // Plugins
+		// 'users.php', // Users
+		// 'tools.php', // Tools
+		// 'options-general.php', // Settings
+		// 'separator-last', // Last separator
+	);
 }
-add_action( 'admin_menu', 'mb_remove_menu_pages' );
 
+/**
+ * Hide Admin Areas that are not used
+ */
+function mb_remove_menu_pages() {
+	// remove_menu_page('link-manager.php');
+}
 
-// Change Admin Menu Order
-// function mb_custom_menu_order($menu_ord) {
-// 	if (!$menu_ord) return true;
-// 	return array(
-// 		'index.php', // Dashboard
-// 		'separator1', // First separator
-// 		'edit.php?post_type=page', // Pages
-// 		'edit.php', // Posts
-// 		'upload.php', // Media
-// 		'gf_edit_forms', // Gravity Forms
-// 		'genesis', // Genesis
-// 		'edit-comments.php', // Comments
-// 		'separator2', // Second separator
-// 		'themes.php', // Appearance
-// 		'plugins.php', // Plugins
-// 		'users.php', // Users
-// 		'tools.php', // Tools
-// 		'options-general.php', // Settings
-// 		'separator-last', // Last separator
-// 	);
-// }
-// add_filter('custom_menu_order', 'mb_custom_menu_order');
-// add_filter('menu_order', 'mb_custom_menu_order');
-
-
-// Remove default link for images
+/**
+ * Remove default link for images
+ */
 function mb_imagelink_setup() {
 	$image_set = get_option( 'image_default_link_type' );
 	if ($image_set !== 'none') {
 		update_option('image_default_link_type', 'none');
 	}
 }
-add_action('admin_init', 'mb_imagelink_setup', 10);
+
+/**
+ * Show Kitchen Sink in WYSIWYG Editor
+ */
+function mb_unhide_kitchensink($args) {
+	$args['wordpress_adv_hidden'] = false;
+	return $args;
+}
 
 
-// Remove Query Strings From Static Resources
+/****************************************
+Frontend
+*****************************************/
+
+/**
+ * HTML5 DOCTYPE
+ * removes the default Genesis doctype, adds new html5 doctype
+*/
+function mb_html5_doctype() {
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<?php
+}
+
+/**
+ * Load custom favicon to header
+ */
+function mb_custom_favicon_filter( $favicon_url ) {
+	return get_stylesheet_directory_uri() . '/images/favicon.ico';
+}
+
+/**
+ * Responsive Meta Tag
+ */
+function mb_viewport_meta_tag() {
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+}
+
+/**
+ * Footer
+ */
+function mb_footer() {
+	echo '<p class="copyright">&copy; ';
+	echo date("Y") . " ";
+	bloginfo('name');
+	echo '</p>';
+}
+
+/**
+ * Enqueue Script
+ */
+function mb_scripts() {
+	if ( !is_admin() ) {
+    	// Modernizr
+		wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js', false, NULL );
+		// Custom plugins and scripts
+		wp_enqueue_script('customplugins', get_stylesheet_directory_uri() . '/js/plugins.min.js', array('jquery'), NULL, true );
+		wp_enqueue_script('customscripts', get_stylesheet_directory_uri() . '/js/main.min.js', array('jquery'), NULL, true );
+	}
+}
+
+/**
+ * Remove Query Strings From Static Resources
+ */
 function mb_remove_script_version($src){
 	$parts = explode('?', $src);
 	return $parts[0];
 }
-add_filter('script_loader_src', 'mb_remove_script_version', 15, 1);
-add_filter('style_loader_src', 'mb_remove_script_version', 15, 1);
 
-
-// Remove Read More Jump
+/**
+ * Remove Read More Jump
+ */
 function mb_remove_more_jump_link($link) {
 	$offset = strpos($link, '#more-');
 	if ($offset) {
@@ -78,14 +200,16 @@ function mb_remove_more_jump_link($link) {
 	}
 	return $link;
 }
-add_filter('the_content_more_link', 'mb_remove_more_jump_link');
 
 
-// Show Kitchen Sink in WYSIWYG Editor
-function mb_unhide_kitchensink($args) {
-	$args['wordpress_adv_hidden'] = false;
-	return $args;
+/****************************************
+Misc Theme Functions
+*****************************************/
+
+/**
+ * Unregister the superfish scripts
+ */
+function mb_unregister_superfish() {
+	wp_deregister_script( 'superfish' );
+	wp_deregister_script( 'superfish-args' );
 }
-add_filter( 'tiny_mce_before_init', 'mb_unhide_kitchensink' );
-
-?>
